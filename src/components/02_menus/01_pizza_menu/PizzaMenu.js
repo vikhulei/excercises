fd02b9a378babb95b5749fc8820031cd5b1fdde3
Viktor Hulei <vikhulei@gmail.com>
2022-10-22 17:42:08 +0000

    PizzaMenu started

---
+++ src/components/02_menus/01_pizza_menu/PizzaMenu.js
@@ -0,0 +1,15 @@
+import {
+  NavBar,
+  LogoImageContainer,
+  MenuItemsContainer,
+} from "./PizzaMenuStyle";
+
+const PizzaMenu = () => {
+  return (
+    <NavBar>
+      <h1>Hello</h1>
+    </NavBar>
+  );
+};
+
+export default PizzaMenu;
